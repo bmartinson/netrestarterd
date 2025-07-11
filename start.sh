67c28e5b6c9cd1b82c5e42ad20792090b7d1026e
bmartinson <brian@brianmartinson.com>
2025-07-10 17:53:29 -0700

    Startup Safety

--- start.sh
+++ start.sh
@@ -12,6 +12,17 @@ echo "Starting daemon..."
 sudo pkill -9 netrestarterd
 
 # Load the daemon
+if [ ! -f /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist ]; then
+  echo "  Plist file not found at /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist"
+  exit 2
+fi
+
+# Make sure permissions are for sure correct
+sudo chmod 644 /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist
+
+# Unload the daemon and load it in case it is already loaded.
+launchctl unload /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist 2>/dev/null
 launchctl load /Library/LaunchDaemons/com.bmartinson.netrestarterd.plist
 
 if [ $? -eq 0 ]; then
